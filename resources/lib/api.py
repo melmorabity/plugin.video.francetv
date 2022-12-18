8503b9406d0a13bbbc780bfede83d6a12cba5808
Mohamed El Morabity <melmorabity@fedoraproject.org>
2022-12-18 11:14:07 +0100

    Enable artwork for special live event channels

--- resources/lib/api.py
+++ resources/lib/api.py
@@ -179,7 +179,8 @@ class FranceTV:
 
         art = {}  # type: Art
 
-        channel_icon = _CHANNEL_ICONS.get(FranceTV._get_channel_id(item))
+        channel = FranceTV._get_channel_id(item)
+        channel_icon = _CHANNEL_ICONS.get(channel)
         art.setdefault("icon", channel_icon)
 
         # Use channel logo as thumb for live videos
@@ -190,7 +191,7 @@ class FranceTV:
 
         # Artwork provided by the france.tv API is really bad for
         # channels
-        if item_type == "channel":
+        if item_type == "channel" and channel:
             return art
 
         for image in item.get("images") or []:
